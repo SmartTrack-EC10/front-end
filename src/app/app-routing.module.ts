47772ac80e8444657461ca1a1f8ccc8de9b5c525
Guilherme Sant'Anna <guilherme.sanntana@gmail.com>
2022-07-26 08:06:33 -0300

    Inclusão da tela de email, apenas visual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { HelpComponent } from './pages/help/help.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotificationsComponent } from './pages/notifications/notifications.component';
 import { FarmComponent } from './pages/register/farm/farm.component';
@@ -41,6 +42,10 @@ const routes: Routes = [
   {
     path: 'aboutus',
     component: AboutComponent,
+  },
+  {
+    path: 'contactus',
+    component: HelpComponent,
   },
 ];
 
