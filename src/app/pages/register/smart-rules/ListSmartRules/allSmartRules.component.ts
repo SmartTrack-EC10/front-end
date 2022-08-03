88e2496dcd9ef4c7a29a1fbb64f2cf417e5e4edc
Guilherme Sant'Anna <guilherme.sanntana@gmail.com>
2022-08-03 14:37:31 -0300

    Alteração do visual do smartrules

--- src/app/pages/register/smart-rules/ListSmartRules/allSmartRules.component.ts
+++ src/app/pages/register/smart-rules/ListSmartRules/allSmartRules.component.ts
@@ -2,27 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { finalize } from 'rxjs';
 import { SmartRuleApiService } from 'src/app/core/services/smart-rules-service/smart-rule-api.service';
 
 @Component({
   selector: 'all-smart-rules',
   templateUrl: './allSmartRules.component.html',
-  styleUrls: ['./allSmartRules.component.scss']
+  styleUrls: ['./allSmartRules.component.scss'],
 })
-
-export class AllSmartRulesComponent implements OnInit  {
-  displayedColumns: string[] = ["description", "status", "expired"]
-  smartRulesList: any[] = []
+export class AllSmartRulesComponent implements OnInit {
+  displayedColumns: string[] = ['description', 'status', 'expires', 'options'];
+  dataSource: any[] = [];
+  smartRulesList: any[] = [];
   isValidSRList: boolean = true;
 
   constructor(
     private fb: FormBuilder,
     private service: SmartRuleApiService,
     private route: Router,
-    private spinner: NgxSpinnerService) 
-  {
-    
-  }
+    private spinner: NgxSpinnerService
+  ) {}
 
   ngOnInit(): void {
     this.getSubiscriptionList(0);
@@ -31,16 +30,21 @@ export class AllSmartRulesComponent implements OnInit  {
   //get a list of SmartRules
   getSubiscriptionList(offset: number) {
     var listServer: any[] = [];
-    this.service.getSmartRules(offset).subscribe(data => {
-        listServer = data;
-        if(listServer.length == 0 ) { this.isValidSRList = false } //disable butons if all data is already downloaded
-        else { listServer.forEach(smartRule => { this.smartRulesList.push(smartRule) }); }
-    });
+    this.spinner.show();
+    this.service
+      .getSmartRules(offset)
+      .pipe(finalize(() =>  this.spinner.hide()))
+      .subscribe((res) => {
+        this.dataSource = res;
+      });
+
+
+
   }
 
   //load more SmartRules from Server
   loadMoreSubscriptions() {
-    var smartRules = this.smartRulesList.length;
+    var smartRules = this.dataSource.length;
     this.getSubiscriptionList(smartRules);
   }
 }
